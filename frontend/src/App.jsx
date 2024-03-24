897eef22f9c56f501c549b43a2e01d00b91d6411
James Flaherty <jamesflaherty@wirelessprv-10-192-197-178.near.illinois.edu>
2024-03-24 15:31:45 -0500

    Initial frontend commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoginPage from './components/LoginPage/LoginPage'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginPage.css'
+const LoginPage = () => {
+  return (
+    <div>
+      <div className='test' style={{ backgroundImage: "url(https://static1.bigstockphoto.com/6/5/2/large1500/256703563.jpg" }}>
+        <button className='button'> 
+          <span>Getting Started</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage

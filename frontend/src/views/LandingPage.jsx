26aca2b908f22370e08e4c11fa300a9ba6a80a3a
jampflah <jamespf3@illinois.edu>
2024-04-15 13:25:10 -0500

    Landing, login and signup page.

---
+++ frontend/src/views/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import image from "../img/landing_page.png"
+import "../css/LandingPage.css"
+
+const LandingPage = () => {
+    const navigate = useNavigate(); // Move the hook call inside the component function
+
+    const handleClick = () => {
+        // Redirect to login
+        navigate('/login');
+    }
+
+    return (
+        <div className="background" style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat"}}>
+            <div className="landing-title">Illini CMS</div>
+            <div className="landing-desc">A Classroom Management System</div>
+            <div className="vertical-space"></div>
+            <button className="start-button" onClick={handleClick}>Get Started</button>
+        </div>
+    );
+}
+
+export default LandingPage;

3d8cd1340a3a9e5d96aae264f8fa92b2ebd4be05
panshuljindal <panshuljindal@gmail.com>
2024-04-30 21:07:23 -0500

    Changes done

--- backend/src/controllers/ClassroomGroupController.ts
+++ backend/src/controllers/ClassroomGroupController.ts
@@ -48,11 +48,10 @@ class ClassroomGroupController{
         return apiResponse("Course added in Classroom", RESPONSE.HTTP_CREATED, {}, res);
     }
 
-    static deleteClassroomGroup(req, res, next){
+    static async deleteClassroomGroup(req, res, next){
         const userId = req.body.userId;
         const classGroupId = req.body.classGroupId;
-        const userResponse: any = SQLHelper.executeQuery(SQLHelper.getUserById(userId));
-        console.log(userResponse);
+        const userResponse: any = await SQLHelper.executeQuery(SQLHelper.getUserById(userId));
         if(userResponse === null || userResponse[0].length === 0){
             return apiResponse("User not found", RESPONSE.HTTP_NOT_FOUND, {}, res);
         }
@@ -60,12 +59,12 @@ class ClassroomGroupController{
         if(user.userType !== USER_TYPES.admin){
             return apiResponse("Only admins are allowed to delete groups", RESPONSE.HTTP_UNAUTHORIZED, {}, res);
         }
-        const classroomGroupResponse: any = SQLHelper.executeQuery(SQLHelper.getClassroomGroupById(classGroupId));
+        const classroomGroupResponse: any = await SQLHelper.executeQuery(SQLHelper.getClassroomGroupById(classGroupId));
         if(classroomGroupResponse === null || classroomGroupResponse[0].length === 0){
             return apiResponse("ClassroomGroup not found", RESPONSE.HTTP_NOT_FOUND, {}, res);
         }
         const classroomGroup = classroomGroupResponse[0][0];
-        const classroomGroupDeleteResponse: any = SQLHelper.executeQuery(SQLHelper.deleteClassroomGroup(classGroupId, classroomGroup.classroomId, classroomGroup.courseId));
+        const classroomGroupDeleteResponse: any = await SQLHelper.executeQuery(SQLHelper.deleteClassroomGroup(classGroupId, classroomGroup.classroomId, classroomGroup.courseId));
         if(classroomGroupDeleteResponse === null){
             return apiResponse("Error in deleting ClassroomGroup", RESPONSE.HTTP_INTERNAL_SERVER_ERROR, {}, res);
         }
